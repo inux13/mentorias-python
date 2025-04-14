608cbc0e1da2f419444ed2eb15343d247b23ea69
Mayara <mayarapedrazoli@gmail.com>
2025-04-14 15:38:59 -0300

    Finalização Aulas

--- mentorados/admin.py
+++ mentorados/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Navigators, Mentorados, DisponibilidadeHorarios
+from .models import Navigators, Mentorados, DisponibilidadeHorarios, Reuniao, Tarefa
 # Register your models here.
 
 admin.site.register(Navigators)
 admin.site.register(Mentorados)
 admin.site.register(DisponibilidadeHorarios)
+admin.site.register(Reuniao)
+admin.site.register(Tarefa)
--- mentorados/views.py
+++ mentorados/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
-from .models import Mentorados, Navigators, DisponibilidadeHorarios, Reuniao
+from .models import Mentorados, Navigators, DisponibilidadeHorarios, Reuniao, Tarefa, Upload
 from django.contrib import messages
 from django.contrib.messages import constants
 from datetime import datetime, timedelta
 from .auth import valida_token
+from  django.views.decorators.csrf import csrf_exempt   
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def mentorados(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-    
-
     if request.method == 'GET':
         navigators = Navigators.objects.filter(user=request.user)
         mentorados = Mentorados.objects.filter(user=request.user)
@@ -44,10 +43,12 @@ def mentorados(request):
 
         messages.add_message(request, constants.SUCCESS, 'Mentorado cadastrado com sucesso.')
         return redirect('mentorados')
-    
+
+@login_required    
 def reunioes(request):
     if request.method == 'GET':
-        return render(request, 'reunioes.html')
+        reunioes= Reuniao.objects.filter(data__mentor=request.user)
+        return render(request, 'reunioes.html', {'reunioes': reunioes})
     elif request.method == 'POST':
         data = request.POST.get('data')
         data = datetime.strptime(data, '%Y-%m-%dT%H:%M')
@@ -109,10 +110,14 @@ def agendar_reuniao(request):
     if not valida_token(request.COOKIES.get('auth_token')):
         return redirect('auth_mentorado')
     
+    mentorado = valida_token(request.COOKIES.get('auth_token'))
+
+    #todo: validar se o horario agendado é realmente de um mentor do mentorado
+
     if request.method == 'GET':
         data = request.GET.get('data')
         data = datetime.strptime(data, '%d-%m-%Y')
-        mentorado = valida_token(request.COOKIES.get('auth_token'))
+        
         horarios = DisponibilidadeHorarios.objects.filter(
             data_inicial__gte=data,
             data_inicial__lt=data + timedelta(days=1),
@@ -121,3 +126,79 @@ def agendar_reuniao(request):
         )
 
         return render(request, 'agendar_reuniao.html', {'horarios':horarios, 'tags': Reuniao.tag_choices})
+
+    else:
+        horario_id = request.POST.get('horario')
+        tag = request.POST.get('tag')
+        descricao = request.POST.get('descricao')
+
+        reuniao = Reuniao(
+            data_id=horario_id,
+            mentorado=mentorado,
+            tag=tag,
+            descricao=descricao
+        )
+        reuniao.save()
+
+        horario = DisponibilidadeHorarios.objects.get(id=horario_id)
+        horario.agendado=True
+        horario.save()
+
+        messages.add_message(request, constants.SUCCESS, 'REUNIÃO AGENDADA COM SUCESSO')
+        return redirect('escolher_dia')
+    
+def tarefa(request, id):
+    mentorado = Mentorados.objects.get(id=id)
+    if mentorado.user != request.user:
+        raise Http404()
+    
+    if request.method == 'GET':
+        tarefas = Tarefa.objects.filter(mentorado=mentorado)
+        videos = Upload.objects.filter(mentorado=mentorado)
+        return render(request, 'tarefa.html', {'mentorado': mentorado, 'tarefas': tarefas, 'videos':videos})
+    
+    else:
+        tarefa = request.POST.get('tarefa')
+
+        tarefa = Tarefa(
+            mentorado=mentorado,
+            tarefa=tarefa
+        )
+        tarefa.save()
+        return redirect(f'/mentorados/tarefa/{id}')
+    
+def upload(request, id):
+    mentorado = Mentorados.objects.get(id=id)
+    if mentorado.user != request.user:
+        raise Http404()
+    
+    video = request.FILES.get('video')
+    upload = Upload(
+        mentorado=mentorado,
+        video=video
+    )
+    upload.save()
+    return redirect(f'/mentorados/tarefa/{id}')
+
+def tarefa_mentorado(request):
+    mentorado = valida_token(request.COOKIES.get('auth_token'))
+    if not mentorado:
+        return redirect('auth_mentorado')
+    
+    if request.method == 'GET':
+        videos = Upload.objects.filter(mentorado=mentorado)
+        tarefas = Tarefa.objects.filter(mentorado=mentorado)
+        return render(request, 'tarefa_mentorado.html', {'mentorado':mentorado,'videos':videos, 'tarefas':tarefas })
+    
+
+@csrf_exempt    
+def tarefa_alterar(request, id):
+    mentorado = valida_token(request.COOKIES.get('auth_token'))
+    if not mentorado:
+        return redirect('auth_mentorado')
+    
+    
+    tarefa = Tarefa.objects.get(id=id)
+    tarefa.realizada = not tarefa.realizada
+    tarefa.save()
+    return HttpResponse('teste')
